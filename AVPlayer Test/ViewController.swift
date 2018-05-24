ab9d3b5d1add2f6c5a37a08bf7b4219c69c2780c
Dylan Gallup <dylangallup@dylans-macbook-pro.local>
2018-05-24 14:25:14 -0700

    Initial Commit

---
+++ AVPlayer Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVPlayer Test
+//
+//  Created by Dylan Gallup on 5/24/18.
+//  Copyright © 2018 Dylan Gallup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
